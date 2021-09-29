0e6aa8551baf95bd59e5f946e40b802fe5e68882
DT3264 <cernadaniel32@gmail.com>
2021-09-28 22:04:22 -0500

    Agregado contenido de Temas

--- app/src/main/java/com/example/estilos/MainActivity.java
+++ app/src/main/java/com/example/estilos/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.estilos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button3);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(this, ActivityTemas.class);
+            startActivity(intent);
+        });
     }
 }
